20bf051ad47192bbe40bf201b1b65d871e3a02ba
Yohann01 <82199055+Yohann01@users.noreply.github.com>
2024-02-06 18:00:55 +0800

    fix: edit layout

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -5,40 +5,57 @@
             @csrf
             @method('PUT')
             <div class="row mb-3">
-
                 <label class="col-sm-2 col-form-label">Employee Number</label>
                 <div class="col-sm-6">
                     <input type="text" class="form-control" name="employee_number" value="{{$employee->employee_number}}">
+                    @error('employee_number')
+                    <p class="text-danger">{{$message}}</p>
+                @enderror
                 </div>
             </div>
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label">First Name</label>
                 <div class="col-sm-6">
                     <input type="text" class="form-control" name="first_name" value="{{$employee->first_name}}">
+                    @error('first_name')
+                    <p class="text-danger">{{$message}}</p>
+                    @enderror
                 </div>
             </div>
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label">Last Name</label>
                 <div class="col-sm-6">
                     <input type="text" class="form-control" name="last_name" value="{{$employee->last_name}}">
+                    @error('last_name')
+                    <p class="text-danger">{{$message}}</p>
+                    @enderror
                 </div>
             </div>
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label">Birthdate</label>
                 <div class="col-sm-6">
                     <input type="date" class="form-control" name="birthdate" value="{{$employee->birthdate}}">
+                    @error('birthdate')
+                    <p class="text-danger">{{$message}}</p>
+                    @enderror
                 </div>
             </div>
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label">Contact Number</label>
                 <div class="col-sm-6">
                     <input type="text" class="form-control" name="contact_number" value="{{$employee->contact_number}}">
+                    @error('contact_number')
+                    <p class="text-danger">{{$message}}</p>
+                    @enderror
                 </div>
             </div>
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-6">
                     <input type="text" class="form-control" name="email" value="{{$employee->email}}">
+                    @error('email')
+                    <p class="text-danger">{{$message}}</p>
+                    @enderror
                 </div>
             </div>
 
